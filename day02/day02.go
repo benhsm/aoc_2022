1c453c180bdf525c441d5bb11ad8bc665b468bfa
Benjamin Ang <ben.ang@fastmail.com>
2022-12-03 00:02:56 -0500

    D2: implement solution for part 2

--- day02/day02.go
+++ day02/day02.go
@@ -18,10 +18,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("Could not read strategy guide: %v", err)
 	}
-	fmt.Println("Your score based on the strategy guide is: ", rps(data))
+	fmt.Println("Your score based on the strategy guide is: ", rps(data, calcScore))
+	fmt.Println("Your real score based on the strategy guide is: ", rps(data, calcScore2))
 }
 
-func rps(strategy []byte) int {
+func rps(strategy []byte, scoreFunc func(t, y byte) int) int {
 	strategy = bytes.TrimRight(strategy, "\n") // remove trailing newline
 	rounds := bytes.Split(strategy, []byte("\n"))
 	var score int
@@ -30,7 +31,7 @@ func rps(strategy []byte) int {
 		if !fnd {
 			log.Fatalf("could not parse input as round in rps game: %v", r)
 		}
-		score += calcScore(theirMove[0], yourMove[0])
+		score += scoreFunc(theirMove[0], yourMove[0])
 	}
 	return score
 }
@@ -46,4 +47,29 @@ func calcScore(theirMove, yourMove byte) int {
 		score += 0
 	}
 	return score
+}
+
+var winningMove = map[byte]byte{
+	byte('A'): byte('Y'),
+	byte('B'): byte('Z'),
+	byte('C'): byte('X'),
+}
+
+var losingMove = map[byte]byte{
+	byte('A'): byte('Z'),
+	byte('B'): byte('X'),
+	byte('C'): byte('Y'),
+}
+
+func calcScore2(theirMove, action byte) int {
+	var yourMove byte
+	switch action {
+	case byte('X'): // lose
+		yourMove = losingMove[theirMove]
+	case byte('Z'): // win
+		yourMove = winningMove[theirMove]
+	default: // draw
+		yourMove = theirMove + 23
+	}
+	return calcScore(theirMove, yourMove)
 }
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -7,8 +7,19 @@ func TestRPS(t *testing.T) {
 B X
 C Z
 `)
-	score := rps(strategy)
+	score := rps(strategy, calcScore)
 	if score != 15 {
 		t.Errorf("wanted score 15, got %d", score)
+	}
+}
+
+func TestRPS2(t *testing.T) {
+	strategy := []byte(`A Y
+B X
+C Z
+`)
+	score := rps(strategy, calcScore2)
+	if score != 12 {
+		t.Errorf("wanted score 12, got %d", score)
 	}
 }
